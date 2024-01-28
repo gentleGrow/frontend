b6f999c3ecaa9d788c2a30bc2fe32f77e23bdfb8
kcw2297 <kcw2371@gmail.com>
2024-01-28 22:39:52 +0900

    user/items 별도 router 분리

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# [정보] 엔진명://user/pw@서버명/db명
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres/1234@assetManagement/postgres"
+# [정보] 엔진명://user/pw@서버주소/db명
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost/assetManagement"
 
 # [정보] create_engine > db 연결을 위한 객체 생성을 합니다. 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -13,3 +13,11 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # [정보] 모든 ORM 클래스에 전달될 기본 클래스입니다.
 Base = declarative_base()
+
+#[정보] 새로운 세션을 연결시킵니다. yield를 통해서 세션 context를 핸들링합니다.
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
