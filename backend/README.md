5d5d9d8f84d5e7ccfa2d5603f5d3bcb511478ee1
kcw2297 <kcw2371@gmail.com>
2024-02-24 21:26:19 +0900

    [프론트] 프론트 폴더 생성

--- backend/README.md
+++ backend/README.md
@@ -43,4 +43,7 @@
     - 로직 적용 > 정렬, 변환
 - [참조] https://www.youtube.com/watch?v=r03ObslCNlo
 
-
+### 코드 커밋
+- [gitflow] main > develop > release > feature/내용 > hotfix * 단점 : ci/cd에 적합하지 않음, 장점 : 안전함
+- [trun-base] main > feature * 단점 : 테스트 자동화 요구, 장점 : 적은 브런치 관리
+- [참조] https://www.youtube.com/watch?v=EV3FZ3cWBp8
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,7 +6,8 @@ export default function Home() {
    
     const REST_API_KEY = process.env.REST_API_KEY;
     const REDIRECT_URI = process.env.REDIRECT_URI;
-    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    // const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    const KAKAO_AUTH_URL = 'http://127.0.0.1:8000/api/auth/kakao';
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
