ff35fcbe09e994f41a3560802cbd808745151486
kcw2297 <kcw2371@gmail.com>
2024-05-29 19:26:59 +0900

    GENTLEGROW-38 quickfix : temp commit purpose

--- backend/etc/cronjob/setup.sh
+++ backend/etc/cronjob/setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-mkdir -p /home/logs
+mkdir -p /code/logs
 
-/usr/bin/python3 ./data/stock/stock_codes.py
-/usr/bin/python3 ./data/stock/stock_codes.py >> /home/logs/stock_codes.log 2>&1
+/usr/bin/python3 /code/data/stock/stock_codes.py >> /code/logs/stock_codes.log 2>&1
 
 echo "초기 셋업에 성공하였습니다."
