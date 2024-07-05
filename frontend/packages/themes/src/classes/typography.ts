f0866baf779717ad5c387e0420a9c1f661122106
soneelee <shine.p715@gmail.com>
2024-07-06 08:09:49 +0900

    GROW-21: feat. 타이포그라피, 박스 토큰

---
+++ frontend/packages/themes/src/classes/typography.ts
@@ -0,0 +1,9 @@
+import { typography } from "../variables";
+
+export const heading = {
+  "4xl": {
+    fontSize: typography.fontSize[72],
+    fontWeight: typography.fontWeight[700],
+    lineHeight: typography.lineHeight[110],
+  },
+};
