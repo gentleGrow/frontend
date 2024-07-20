e30a8f77e093418950a8d76e37d3761feeabeadf
kcw2297 <kcw2371@gmail.com>
2024-07-20 15:05:53 +0900

    GROW35 feat : kakao authenticate logic with test code

--- backend/app/api/auth/v1/router.py
+++ backend/app/api/auth/v1/router.py
@@ -7,7 +7,7 @@ from app.module.auth.jwt import JWTBuilder
 from app.module.auth.model import User
 from app.module.auth.repository import UserRepository
 from app.module.auth.schema import AccessTokenResponse, TokenRefreshRequest, TokenRequest, TokenResponse
-from app.module.auth.service import Google
+from app.module.auth.service import Google, Kakao
 from database.dependency import get_router_sql_session
 from database.redis import redis_repository
 
@@ -15,8 +15,48 @@ auth_router = APIRouter(prefix="/v1")
 
 
 @auth_router.post(
+    "/kakao",
+    summary="client 카카오 id token을 확인후 jwt 토큰을 반환합니다.",
+    description="client 카카오 토큰이 넘어오는 지 확인후, 해당 토큰으로 유저확인을 합니다. 신규 유저인 경우 DB에 저장한후, jwt를 반환합니다.",
+    response_model=TokenResponse,
+)
+async def kakao_login(request: TokenRequest, session: AsyncSession = Depends(get_router_sql_session)) -> TokenResponse:
+    id_token = request.id_token
+    if not id_token:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="카카오 id token이 넘어오지 않았습니다.",
+        )
+
+    try:
+        id_info = await Kakao.verify_token(id_token)
+    except ValueError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+    social_id = id_info.get("sub")
+    if social_id is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="id token에 유저 정보가 없습니다.")
+
+    user = await UserRepository.get_by_social_id(session, social_id, ProviderEnum.KAKAO)
+    if user is None:
+        user = User(
+            social_id=social_id,
+            provider=ProviderEnum.KAKAO.value,
+        )
+        user = await UserRepository.create(session, user)
+
+    access_token = JWTBuilder.generate_access_token(user.id)
+    refresh_token = JWTBuilder.generate_refresh_token(user.id)
+
+    user_string_id = str(user.id)
+    await redis_repository.save(user_string_id, refresh_token, REDIS_JWT_REFRESH_EXPIRE_TIME_SECOND)
+
+    return TokenResponse(access_token=access_token, refresh_token=refresh_token)
+
+
+@auth_router.post(
     "/google",
-    summary="client 구글 token을 확인후 jwt 토큰을 반환합니다.",
+    summary="client 구글 id token을 확인후 jwt 토큰을 반환합니다.",
     description="client 구글 토큰이 넘어오는 지 확인후, 해당 토큰으로 유저확인을 합니다. 신규 유저인 경우 DB에 저장한후, jwt를 반환합니다.",
     response_model=TokenResponse,
 )
--- backend/app/module/auth/service.py
+++ backend/app/module/auth/service.py
@@ -1,11 +1,30 @@
 from os import getenv
 
+import httpx
 from dotenv import load_dotenv
+from fastapi import status
 from google.auth.transport import requests
 from google.oauth2 import id_token
 
 load_dotenv()
+
 GOOGLE_CLIENT_ID = getenv("GOOGLE_CLIENT_ID", None)
+
+KAKAO_TOKEN_INFO_URL = getenv("KAKAO_TOKEN_INFO_URL", None)
+
+
+class Kakao:
+    @staticmethod
+    async def verify_token(id_token: str) -> dict:
+        data = {"id_token": id_token}
+        headers = {"Content-Type": "application/x-www-form-urlencoded;charset=utf-8"}
+        async with httpx.AsyncClient() as client:
+            response = await client.post(KAKAO_TOKEN_INFO_URL, data=data, headers=headers)
+            if response.status_code is not status.HTTP_200_OK:
+                error_response = response.json()
+                raise ValueError(error_response.get("error_description"))
+            else:
+                return response.json()
 
 
 class Google:
