a01582e4ec3a6d87a874c195f726634070ab7005
soneelee <shine.p715@gmail.com>
2024-08-03 14:20:43 +0900

    GROW-36: 버튼 컴포넌트 - 로딩중, 아이콘

--- frontend/packages/react/components/button/src/Button.tsx
+++ frontend/packages/react/components/button/src/Button.tsx
@@ -7,6 +7,8 @@ import {
   buttonStyle,
   enableColorVariant,
   hoverColorVariant,
+  spanStyle,
+  spinnerStyle,
 } from "./style.css";
 import { vars } from "@gaemi-school/themes";
 
@@ -15,9 +17,12 @@ const Button = (props: ButtonProps, ref: React.Ref<HTMLButtonElement>) => {
     variant = "solid",
     size = "md",
     color = "gray",
-
+    leftIcon,
+    rightIcon,
+    isLoading,
     isDisabled = false,
     children,
+    onKeyDown,
     style,
   } = props;
 
@@ -31,18 +36,32 @@ const Button = (props: ButtonProps, ref: React.Ref<HTMLButtonElement>) => {
       ? vars.colors.$scale[color][70]
       : vars.colors.$scale[color][10];
 
-  const disabled = isDisabled;
+  const disabled = isDisabled || isLoading;
 
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLButtonElement>) => {
+    onKeyDown?.(event);
+
+    if (event.key === "Enter" || event.key === "13") {
+      event.preventDefault();
+      event.currentTarget.click();
+    }
+  };
   return (
     <button
       {...props}
       ref={ref}
+      onKeyDown={handleKeyDown}
+      onClick={() => {
+        console.log("ttt");
+      }}
+      role="button"
       className={clsx([
         buttonStyle({
           size,
           variant,
         }),
       ])}
+      data-loading={isLoading}
       disabled={disabled}
       style={{
         ...assignInlineVars({
@@ -53,7 +72,10 @@ const Button = (props: ButtonProps, ref: React.Ref<HTMLButtonElement>) => {
         ...style,
       }}
     >
-      {children}
+      {isLoading && <div className={spinnerStyle({ size })} />}
+      {leftIcon && <span className={spanStyle({ size })}>{leftIcon}</span>}
+      <span>{children}</span>
+      {rightIcon && <span className={spanStyle({ size })}>{rightIcon}</span>}
     </button>
   );
 };
