f376c88241fe1500c81d8ed74e36f35672627cf0
KimYouGyoung <tptkds12@gmail.com>
2024-08-16 00:52:15 +0900

    GROW-55 feat: Login(Login Modal) 컴포넌트

--- frontend/src/shared/ui/LineButton.tsx
+++ frontend/src/shared/ui/LineButton.tsx
@@ -14,20 +14,21 @@ export default function LineButton({
   props?: Record<string, any>;
   children?: ReactNode;
 }) {
+  const { className, ...restProps } = props || {};
+
+  const finalClassName = clsx(
+    "relative h-[48px] w-[394px] rounded-md border text-center text-[16px] font-semibold leading-[24px] text-[#2A2D31] disabled:text-[#B9BCC1]",
+    {
+      "hover:border-[#999DA4]": !isDisabled,
+      "border-[#D8DADC]": !isHover && !isDisabled,
+      "border-[#999DA4]": isHover,
+      "border-[#D8DADC] text-[#B9BCC1]": isDisabled,
+    },
+    className,
+  );
+
   return (
-    <button
-      className={clsx(
-        "relative h-[48px] w-[394px] rounded-md border text-center text-[16px] font-semibold leading-[24px] text-[#2A2D31] disabled:text-[#B9BCC1]",
-        {
-          "hover:border-[#999DA4]": !isDisabled,
-          "border-[#D8DADC]": !isHover && !isDisabled,
-          "border-[#999DA4]": isHover,
-          "border-[#D8DADC] text-[#B9BCC1]": isDisabled,
-        },
-      )}
-      disabled={isDisabled}
-      {...props}
-    >
+    <button className={finalClassName} disabled={isDisabled} {...restProps}>
       <div className="absolute left-[16px] top-1/2 -translate-y-1/2">
         {children}
       </div>
