3654769224ae9894dad6b64a321d0ae5d8854b19
kcw2297 <kcw2371@gmail.com>
2024-08-16 23:42:11 +0900

    GROW-53 feat : eventbridge 스케줄링을 위한 일/주/월 별 코드 분리

--- backend/app/data/yahoo/source/constant.py
+++ backend/app/data/yahoo/source/constant.py
@@ -1,9 +1,13 @@
 from app.module.asset.enum import TimeInterval
 from app.module.asset.model import StockDaily, StockMonthly, StockWeekly
 
+STOCK_SCHEDULE_INTERVAL = "7d"
 STOCK_TIME_INTERVAL = "1d"
 STOCK_HISTORY_TIMERANGE_YEAR = 15
 BATCH_SIZE = 100
+
+DAY = "1d"
+
 
 TIME_INTERVAL_MODEL_REPO_MAP = {
     TimeInterval.DAY: StockDaily,
--- backend/app/module/asset/repository/stock_daily_repository.py
+++ backend/app/module/asset/repository/stock_daily_repository.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+from sqlalchemy.dialects.mysql import insert as mysql_insert
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
@@ -25,3 +26,39 @@ class StockDailyRepository:
             select(StockDaily).where(StockDaily.code == stock_code).order_by(StockDaily.date.desc()).limit(1)
         )
         return result.scalar_one_or_none()
+
+    @staticmethod
+    async def bulk_upsert(session: AsyncSession, stock_dailies: list[StockDaily]) -> None:
+        stmt = mysql_insert(StockDaily).values(
+            [
+                {
+                    "code": stock_daily.code,
+                    "date": stock_daily.date,
+                    "opening_price": stock_daily.opening_price,
+                    "highest_price": stock_daily.highest_price,
+                    "lowest_price": stock_daily.lowest_price,
+                    "close_price": stock_daily.close_price,
+                    "adj_close_price": stock_daily.adj_close_price,
+                    "trade_volume": stock_daily.trade_volume,
+                }
+                for stock_daily in stock_dailies
+            ]
+        )
+
+        update_dict = {
+            "opening_price": stmt.inserted.opening_price,
+            "highest_price": stmt.inserted.highest_price,
+            "lowest_price": stmt.inserted.lowest_price,
+            "close_price": stmt.inserted.close_price,
+            "adj_close_price": stmt.inserted.adj_close_price,
+            "trade_volume": stmt.inserted.trade_volume,
+        }
+
+        upsert_stmt = stmt.on_duplicate_key_update(update_dict)
+
+        try:
+            await session.execute(upsert_stmt)
+            await session.commit()
+        except Exception as e:
+            print(f"Error during bulk upsert: {e}")
+            await session.rollback()
