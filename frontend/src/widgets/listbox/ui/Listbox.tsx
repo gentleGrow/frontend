6ae13cb879799cae38a3b7429436cf8a84de83b7
soneelee <shine.p715@gmail.com>
2024-09-15 14:36:11 +0900

    GROW-76: design. Listbox - mt-2 제거

--- frontend/src/widgets/listbox/ui/Listbox.tsx
+++ frontend/src/widgets/listbox/ui/Listbox.tsx
@@ -20,8 +20,8 @@ export default function ListboxComponent({
 
   return (
     <Listbox value={selected} onChange={setSelected}>
-      <div className="relative mt-2">
-        <ListboxButton className="relative w-full cursor-default rounded-md bg-white px-2.5 py-1.5 text-left text-gray-900 shadow-sm ring-0 ring-inset ring-gray-300 focus:outline-none focus:ring-1 focus:ring-green-60 sm:text-sm sm:leading-6">
+      <div className="relative">
+        <ListboxButton className="relative w-full cursor-default rounded-md bg-white px-2.5 py-1.5 text-left text-gray-90 shadow-sm ring-0 ring-inset ring-gray-300 focus:outline-none focus:ring-1 focus:ring-green-60 sm:text-sm sm:leading-6">
           <div className="relative flex h-7 items-center rounded-[100px] border border-gray-20 px-2 py-1">
             <span className="flex items-center">
               {iconPath && (
@@ -53,11 +53,11 @@ export default function ListboxComponent({
             <ListboxOption
               key={option.id}
               value={option}
-              className="group relative cursor-default select-none border-green-60 px-2 py-1 text-gray-900 data-[focus]:border-l-2 data-[focus]:bg-gray-5"
+              className="group relative cursor-default select-none border-green-60 px-2 py-1 text-gray-90 data-[focus]:border-l-2 data-[focus]:bg-gray-5"
             >
               <div className="flex h-7 items-center rounded-[100px] border border-gray-20 px-2 py-1">
                 {iconPath && (
-                  <img
+                  <image
                     src={iconPath}
                     alt="Securities Icon"
                     className="h-4 w-4 flex-shrink-0 rounded-full"
