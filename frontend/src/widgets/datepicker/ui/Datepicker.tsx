f70426cc477907fca0da7a12d19f049d9f79597a
soneelee <shine.p715@gmail.com>
2024-09-17 14:57:58 +0900

    GROW-76: design. Datepicker 컴포넌트 캘린더 CSS

--- frontend/src/widgets/datepicker/ui/Datepicker.tsx
+++ frontend/src/widgets/datepicker/ui/Datepicker.tsx
@@ -2,7 +2,9 @@ import { useState, forwardRef } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { ko } from "date-fns/locale";
-
+import { Button } from "@/shared/ui/button/Button";
+import Image from "next/image";
+import styles from "./styles.module.css";
 // 사용자 정의 입력창 컴포넌트
 const CustomInput = forwardRef((props, ref: React.Ref<HTMLInputElement>) => {
   const { value, onClick } = props as { value: string; onClick: () => void }; // props에서 value와 onClick을 추출합니다.
@@ -32,16 +34,58 @@ const DatepickerComponent = () => {
   };
 
   const handleYearMonthClick = () => {
-    setShowMonthPicker(true);
+    setShowMonthPicker(!showMonthPicker);
   };
 
   const handleCalendarClose = () => {
     setShowMonthPicker(false);
   };
 
+  const isToday = (date: Date) => {
+    const today = new Date();
+    return (
+      date.getDate() === today.getDate() &&
+      date.getMonth() === today.getMonth() &&
+      date.getFullYear() === today.getFullYear()
+    );
+  };
+
+  const renderDayContents = (day: number, date: Date) => {
+    if (isToday(date)) {
+      return (
+        <div className="relative flex items-center justify-center">
+          <div className="absolute -top-0 left-1/2 h-[4px] w-[4px] -translate-x-1/2 rounded-full bg-green-60" />
+          <span>{day}</span>
+        </div>
+      );
+    }
+
+    return (
+      <div className="relative flex items-center justify-center">
+        <span>{day}</span>
+      </div>
+    );
+  };
+
+  const renderMonthContent = (
+    month: number,
+    shortMonthText: string,
+    fullMonthText: string,
+    day: Date,
+  ) => {
+    return (
+      <div className="flex w-full items-center justify-center">
+        <div className="flex h-[36px] w-[36px] items-center justify-center rounded-full">
+          <span>{month + 1}</span>
+        </div>
+      </div>
+    );
+  };
+
   return (
-    <div className="relative">
+    <div className={`relative w-full ${styles.customDatepicker}`}>
       <DatePicker
+        className={styles.customDatepicker}
         selected={selectedDate}
         onChange={
           showMonthPicker ? handleDateChange : (date) => setSelectedDate(date)
@@ -52,13 +96,11 @@ const DatepickerComponent = () => {
         popperPlacement="bottom"
         customInput={<CustomInput />}
         showPopperArrow={false}
-        dayClassName={(date) =>
-          date.getDate() === new Date().getDate()
-            ? "bg-green-500 text-white rounded-full"
-            : ""
-        }
         onCalendarClose={handleCalendarClose}
         shouldCloseOnSelect={showMonthPicker ? false : true}
+        disabledKeyboardNavigation
+        renderDayContents={renderDayContents}
+        renderMonthContent={renderMonthContent}
         renderCustomHeader={({
           date,
           decreaseMonth,
@@ -68,31 +110,42 @@ const DatepickerComponent = () => {
           prevMonthButtonDisabled,
           nextMonthButtonDisabled,
         }) => (
-          <div className="flex items-center justify-between px-2 py-1">
-            <button
+          <div className="flex items-center justify-between bg-white pb-[10px]">
+            <Button
+              variant="icon"
+              size="icon"
+              leftIcon={
+                <Image
+                  src="/images/arrow_left.svg"
+                  alt="left arrow button"
+                  width={20}
+                  height={20}
+                />
+              }
               onClick={showMonthPicker ? decreaseYear : decreaseMonth}
               disabled={prevMonthButtonDisabled}
-              className="px-2 py-1"
-            >
-              {"<"}
-            </button>
+            />
             <button
               type="button"
-              className="text-lg font-semibold"
+              className="rounded-[6px] px-[12px] py-[7px] text-xs font-semibold text-gray-100 hover:bg-gray-10"
               onClick={handleYearMonthClick}
             >
-              {date.toLocaleString("ko-KR", {
-                year: "numeric",
-                month: "long",
-              })}
+              {`${date.getFullYear()}. ${String(date.getMonth() + 1).padStart(2, "0")}`}
             </button>
-            <button
+            <Button
+              variant="icon"
+              size="icon"
+              leftIcon={
+                <Image
+                  src="/images/arrow_right.svg"
+                  alt="right arrow button"
+                  width={20}
+                  height={20}
+                />
+              }
               onClick={showMonthPicker ? increaseYear : increaseMonth}
               disabled={nextMonthButtonDisabled}
-              className="px-2 py-1"
-            >
-              {">"}
-            </button>
+            />
           </div>
         )}
       />
