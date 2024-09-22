42a74eb1a8ba2c39b07002d5c9b7cdac6955b2ce
soneelee <shine.p715@gmail.com>
2024-09-22 22:35:56 +0900

    GROW-76: feat. 자산관리 시트 - 수익률 테이블 셀

--- frontend/src/features/sheet/ui/ProfitCell.tsx
+++ frontend/src/features/sheet/ui/ProfitCell.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Input from "@/shared/ui/Input";
+import { useState } from "react";
 
 export default function ProfitCell({
   value,
@@ -15,5 +16,22 @@ export default function ProfitCell({
       onChange(value);
     }
   };
-  return <Input type="text" value={value} onChange={handleChange} />;
+  return (
+    <>
+      {value && (
+        <div
+          className={`py-\[9\.5px\] relative h-full w-full border-y text-right text-body-sm ${value === 0 ? "border-gray-10 bg-white text-gray-90" : value > 0 ? "text-alet border-[#FFECEC] bg-[#FFECEC]" : "border-[#DCEAFF] bg-[#DCEAFF] text-[#0A6CFF]"}`}
+        >
+          {value}%
+        </div>
+      )}
+      {!value && (
+        <div
+          className={`py-\[9\.5px\] relative h-full w-full border-y border-gray-10 bg-gray-10 text-right text-body-sm text-gray-50`}
+        >
+          자동 계산 필드입니다.
+        </div>
+      )}
+    </>
+  );
 }
