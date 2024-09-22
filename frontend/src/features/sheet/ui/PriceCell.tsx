931b0f4be70475ab8cc4f534a9c5f41653a65b8c
soneelee <shine.p715@gmail.com>
2024-09-22 22:30:36 +0900

    GROW-76: feat. 자산관리 시트 - 금액 입력 테이블 셀

--- frontend/src/features/sheet/ui/PriceCell.tsx
+++ frontend/src/features/sheet/ui/PriceCell.tsx
@@ -1,19 +1,66 @@
 "use client";
 
-import Input from "@/shared/ui/Input";
+import Cell from "@/shared/ui/Cell";
 
 export default function PriceCell({
   value,
   onChange,
+  isEditable,
+  isNew,
+  isKRW,
 }: {
   value?: any;
   onChange?: (value: any) => void;
+  isEditable?: boolean;
+  isNew?: boolean;
+  isKRW?: boolean;
 }) {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
-    if (onChange) {
-      onChange(value);
+    if (/^\d*$/.test(value)) {
+      if (onChange) {
+        onChange(value);
+      }
     }
   };
-  return <Input type="text" value={value} onChange={handleChange} />;
+  return (
+    <>
+      {isEditable && (
+        <div className="relative">
+          <div className="absolute top-1/2 -translate-y-1/2 pl-[10px]"></div>
+          <Cell
+            type="text"
+            value={value}
+            onChange={handleChange}
+            isKRW={isKRW}
+          >
+      <span className="mr-[2px]">{isKRW ? "₩" : "$"}</span>
+
+          </Cell>
+        </div>
+      )}
+      {!isEditable && !isNew && value > 0 && (
+        <div
+          className={`py-\[9\.5px\] relative h-full w-full border-y text-right text-body-sm`}
+        >
+          <span className="mr-[2px]">{isKRW ? "₩" : "$"}</span>
+          <span>{value}</span>
+        </div>
+      )}
+      {!isEditable && !isNew && !value && (
+        <div
+          className={`py-\[9\.5px\] relative h-full w-full border-y border-gray-10 bg-gray-10 text-right text-body-sm text-gray-50`}
+        >
+          배당금이 없는 종목이에요.
+        </div>
+      )}
+      {!isEditable && isNew && (
+        <div
+          className={`py-\[9\.5px\] relative h-full w-full border-y border-gray-10 bg-gray-10 text-right text-body-sm text-gray-50`}
+        >
+          자동 계산 필드입니다.
+        </div>
+      )}
+    </>
+  );
 }
