bc5b9f6e84784bf8606a8b49c64aa95014b78638
soneelee <shine.p715@gmail.com>
2024-09-22 23:30:27 +0900

    GROW-76: fix. IncDecRate 컴포넌트 className 프롭스 추가

--- frontend/src/shared/ui/IncDecRate.tsx
+++ frontend/src/shared/ui/IncDecRate.tsx
@@ -1,8 +1,14 @@
-export default function IncDecRate({ rate }: { rate: number }) {
+export default function IncDecRate({
+  rate,
+  className = "",
+}: {
+  rate: number;
+  className?: string;
+}) {
   return (
     <div
       aria-label={`The rate is ${rate > 0 ? "increased" : rate < 0 ? "decreased" : "unchanged"} by ${rate}%`}
-      className={`flex h-[24px] w-[64px] items-center justify-center rounded-md px-[8px] py-[3px] text-body-4 ${rate === 0 ? "bg-gray-5 text-gray-50" : rate > 0 ? "bg-alert/10 text-alert" : "bg-decrease/10 text-decrease"}`}
+      className={`flex h-[24px] w-[64px] items-center justify-center rounded-md px-[8px] py-[3px] text-body-4 ${rate === 0 ? "bg-gray-5 text-gray-50" : rate > 0 ? "bg-alert/10 text-alert" : "bg-decrease/10 text-decrease"} ${className}`}
     >
       {rate > 0 && "+"}
       {rate}%
