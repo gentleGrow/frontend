636dcbc67f86195cbd5bc65ea700bdced7ad456a
KimYouGyoung <tptkds12@gmail.com>
2024-09-25 20:04:00 +0900

    GROW-77 feat: 투자 성과 API 연동
    - 데일리 투자 성과는 임시로 5일 투자 성과 데이터 반영

--- frontend/src/widgets/investment-performance-chart/api/fetchPerformanceAnalysis.ts
+++ frontend/src/widgets/investment-performance-chart/api/fetchPerformanceAnalysis.ts
@@ -1,22 +1,108 @@
 import { LineChartData, SERVICE_SERVER_URL } from "@/shared";
-
-const fetchPerformanceAnalysis = async (): Promise<LineChartData> => {
+export interface PerformanceAnalysisData {
+  dailyPerformanceData: LineChartData;
+  fiveDayPerformanceData: LineChartData;
+  monthlyPerformanceData: LineChartData;
+  threeMonthPerformanceData: LineChartData;
+  sixMonthPerformanceData: LineChartData;
+  yearlyPerformanceData: LineChartData;
+}
+const fetchPerformanceAnalysis = async (): Promise<PerformanceAnalysisData> => {
   try {
-    const response = await fetch(
-      `${SERVICE_SERVER_URL}/api/chart/v1/sample/performance-analysis`,
-    );
-    if (!response.ok) {
-      throw new Error(`${response.status}: ${await response.json()}`);
+    const [
+      fiveDaysResponse,
+      oneMonthResponse,
+      threeMonthsResponse,
+      sixMonthsResponse,
+      oneYearResponse,
+    ] = await Promise.all([
+      fetch(
+        `${SERVICE_SERVER_URL}/api/chart/v1/sample/performance-analysis?interval=5day`,
+      ),
+      fetch(
+        `${SERVICE_SERVER_URL}/api/chart/v1/sample/performance-analysis?interval=1month`,
+      ),
+      fetch(
+        `${SERVICE_SERVER_URL}/api/chart/v1/sample/performance-analysis?interval=3month`,
+      ),
+      fetch(
+        `${SERVICE_SERVER_URL}/api/chart/v1/sample/performance-analysis?interval=6month`,
+      ),
+      fetch(
+        `${SERVICE_SERVER_URL}/api/chart/v1/sample/performance-analysis?interval=1year`,
+      ),
+    ]);
+
+    if (
+      !fiveDaysResponse.ok ||
+      !oneMonthResponse.ok ||
+      !threeMonthsResponse.ok ||
+      !sixMonthsResponse.ok ||
+      !oneYearResponse.ok
+    ) {
+      throw new Error(
+        `${fiveDaysResponse.status}: ${await fiveDaysResponse.json()}` +
+          `${oneMonthResponse.status}: ${await oneMonthResponse.json()}` +
+          `${threeMonthsResponse.status}: ${await threeMonthsResponse.json()}` +
+          `${sixMonthsResponse.status}: ${await sixMonthsResponse.json()}` +
+          `${oneYearResponse.status}: ${await oneYearResponse.json()}`,
+      );
     }
-    const performanceAnalysis = await response.json();
-    return performanceAnalysis;
+    const [fiveDays, oneMonths, threeMonths, sixMonths, oneYear] =
+      await Promise.all([
+        fiveDaysResponse.json(),
+        oneMonthResponse.json(),
+        threeMonthsResponse.json(),
+        sixMonthsResponse.json(),
+        oneYearResponse.json(),
+      ]);
+    return {
+      dailyPerformanceData: fiveDays as LineChartData,
+      fiveDayPerformanceData: fiveDays as LineChartData,
+      monthlyPerformanceData: oneMonths as LineChartData,
+      threeMonthPerformanceData: threeMonths as LineChartData,
+      sixMonthPerformanceData: sixMonths as LineChartData,
+      yearlyPerformanceData: oneYear as LineChartData,
+    };
   } catch (error) {
     console.error(error);
     return {
-      xAxises: [],
-      values1: { values: [], name: "" },
-      values2: { values: [], name: "" },
-      unit: "",
+      dailyPerformanceData: {
+        xAxises: [],
+        values1: { values: [], name: "" },
+        values2: { values: [], name: "" },
+        unit: "",
+      },
+      fiveDayPerformanceData: {
+        xAxises: [],
+        values1: { values: [], name: "" },
+        values2: { values: [], name: "" },
+        unit: "",
+      },
+      monthlyPerformanceData: {
+        xAxises: [],
+        values1: { values: [], name: "" },
+        values2: { values: [], name: "" },
+        unit: "",
+      },
+      threeMonthPerformanceData: {
+        xAxises: [],
+        values1: { values: [], name: "" },
+        values2: { values: [], name: "" },
+        unit: "",
+      },
+      sixMonthPerformanceData: {
+        xAxises: [],
+        values1: { values: [], name: "" },
+        values2: { values: [], name: "" },
+        unit: "",
+      },
+      yearlyPerformanceData: {
+        xAxises: [],
+        values1: { values: [], name: "" },
+        values2: { values: [], name: "" },
+        unit: "",
+      },
     };
   }
 };
