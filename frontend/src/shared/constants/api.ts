e8ad825397b22e7591ff5786b0da28a930c7b4df
KimYouGyoung <tptkds12@gmail.com>
2024-10-06 00:15:49 +0900

    chore: 불필요한 code 제거

--- frontend/src/shared/constants/api.ts
+++ frontend/src/shared/constants/api.ts
@@ -1,3 +1,2 @@
-export const FRONT_SERVER_URL = "http://localhost:3000";
 export const SERVICE_SERVER_URL = "http://api.gaemischool.com:8000";
 export const RESPONSE_STATUS = { BAD_REQUEST: 400, INTERNAL_SERVER_ERROR: 500 };
