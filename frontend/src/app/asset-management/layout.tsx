011fe6b31918f0d1903263d52d1b27443e842fc9
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-10-09 17:26:59 +0900

    GROW-87 fix: 상단 요약 컴포넌트 위치 layout 으로 이동 및 로딩, 에러 UI 구현

--- frontend/src/app/asset-management/layout.tsx
+++ frontend/src/app/asset-management/layout.tsx
@@ -1,6 +1,9 @@
 "use client"; // Client Component로 선언
 
-import Tabs from "@/widgets/tab/Tabs"; // 새로 분리한 Tab 컴포넌트 가져오기
+import Tabs from "@/widgets/tab/Tabs";
+import Summary from "@/widgets/card/Summary";
+import { Suspense } from "react";
+import { ErrorBoundary } from "next/dist/client/components/error-boundary"; // 새로 분리한 Tab 컴포넌트 가져오기
 
 const AssetManagement: React.FC<{ children: React.ReactNode }> = ({
   children,
@@ -15,6 +18,15 @@ const AssetManagement: React.FC<{ children: React.ReactNode }> = ({
       <div className="header px-[20px]">
         <Tabs items={navItems} />
         {/* timestamp */}
+        <ErrorBoundary
+          errorComponent={({ error }) => (
+            <div>에러가 발생 했습니다.{error.message}</div>
+          )}
+        >
+          <Suspense fallback={<div>로딩중...</div>}>
+            <Summary />
+          </Suspense>
+        </ErrorBoundary>
       </div>
       <div className="content h-[calc(100svh-64px)] min-w-[1080px] overflow-auto bg-gray-5 px-[20px] pt-[20px]">
         {children}
