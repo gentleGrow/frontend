709c36b2bbbdae03a26cee0bd897121c421a3bdb
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-10-10 18:34:48 +0900

    GROW-87 fix: 카드, 가격 표시 컴포넌트 인터페이스 및 세부 구현 수정

--- frontend/src/shared/ui/Card.tsx
+++ frontend/src/shared/ui/Card.tsx
@@ -1,20 +1,37 @@
 import { ReactNode } from "react";
+import { cn } from "@/components/ui/utils";
+
+const fontSizesVariants = {
+  xl: "text-xl",
+  lg: "text-lg",
+  md: "text-md",
+  sm: "text-sm",
+};
 
 interface CardProps {
   children: ReactNode; // children prop의 타입 정의
   title?: string;
+  fontSize?: keyof typeof fontSizesVariants; // fontSize를 위한 prop 추가
   width?: string; // 너비를 위한 prop 추가
   height?: string; // 높이를 위한 prop 추가
 }
 
-export default function Card({ children, title, width, height }: CardProps) {
+export default function Card({
+  children,
+  title,
+  width,
+  height,
+  fontSize = "md",
+}: CardProps) {
   return (
     <div
       className="flex flex-col justify-between rounded-lg border bg-white p-4"
       style={{ width: width, height: height }}
     >
       {title && (
-        <h2 className="text-[16px] font-bold leading-[19px]">{title}</h2>
+        <h2 className={cn("font-bold", fontSizesVariants[fontSize])}>
+          {title}
+        </h2>
       )}
       {children}
     </div>
