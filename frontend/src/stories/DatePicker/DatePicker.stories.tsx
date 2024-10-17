bce3b0c60482aa1c3b690d8e02561087c6db3281
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-10-17 17:10:51 +0900

    GROW-98 feat: 공용 데이트피커 컴포넌트 구현

---
+++ frontend/src/stories/DatePicker/DatePicker.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import DatePicker from "@/shared/ui/DatePicker";
+import { useState } from "react";
+
+const meta: Meta = {
+  title: "React Components/DatePicker",
+  render: () => {
+    const [date, setDate] = useState<Date | null>(null);
+
+    return <DatePicker date={date} onChange={setDate} />;
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof DatePicker>;
+
+export const DatePickerStory: Story = {
+  render: () => {
+    const [date, setDate] = useState<Date | null>(null);
+
+    return <DatePicker date={date} onChange={setDate} />;
+  },
+};
