69ac5acf3ad59fa95a09d830b73cda2c2a8695be
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-10-24 23:17:09 +0900

    GROW-137 feat: 헤더 정렬 기능 구현

--- frontend/src/widgets/asset-management-draggable-table/quries/useGetAssetStocks.ts
+++ frontend/src/widgets/asset-management-draggable-table/quries/useGetAssetStocks.ts
@@ -2,10 +2,59 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import { keyStore } from "@/shared/lib/query-keys";
 import { getAssetsStock } from "@/widgets/asset-management-draggable-table/api/getAssetsStock";
 
-export const useGetAssetStocks = (accessToken: string | null) => {
+export const useGetAssetStocks = (
+  accessToken: string | null,
+  sortOptions: {
+    sortBy: string;
+    sortOrder: "asc" | "desc";
+    type: "date" | "string" | "number";
+  } | null,
+) => {
   return useSuspenseQuery({
     queryKey: keyStore.assetStock.getSummary.queryKey,
     queryFn: () => getAssetsStock(accessToken),
     staleTime: Infinity,
+    select: (data) => {
+      if (!sortOptions) {
+        return data;
+      }
+
+      return {
+        ...data,
+        stock_assets: data.stock_assets.sort((a, b) => {
+          if (sortOptions.sortOrder === "asc") {
+            switch (sortOptions.type) {
+              case "date":
+                return (
+                  new Date(a[sortOptions.sortBy].value).getTime() -
+                  new Date(b[sortOptions.sortBy].value).getTime()
+                );
+              case "string":
+                return a[sortOptions.sortBy].value.localeCompare(
+                  b[sortOptions.sortBy].value,
+                );
+              default:
+                return (
+                  a[sortOptions.sortBy].value - b[sortOptions.sortBy].value
+                );
+            }
+          }
+
+          switch (sortOptions.type) {
+            case "date":
+              return (
+                new Date(b[sortOptions.sortBy].value).getTime() -
+                new Date(a[sortOptions.sortBy].value).getTime()
+              );
+            case "string":
+              return b[sortOptions.sortBy].value.localeCompare(
+                a[sortOptions.sortBy].value,
+              );
+            default:
+              return b[sortOptions.sortBy].value - a[sortOptions.sortBy].value;
+          }
+        }),
+      };
+    },
   });
 };
