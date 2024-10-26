548854a2bfb337ac42bf1bc7af62c36a98fccf04
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-10-27 01:30:48 +0900

    GROW-153: fix: getBaseUrl 함수 수정

--- frontend/src/shared/utils/getBaseUrl.ts
+++ frontend/src/shared/utils/getBaseUrl.ts
@@ -1,6 +1,6 @@
+import { SERVICE_SERVER_URL } from "@/shared";
+
 export function getBaseUrl() {
   if (typeof window !== "undefined") return "";
-  const vc = process.env.VERCEL_URL;
-  if (vc) return `https://${vc}`;
-  return "http://localhost:3000";
+  return SERVICE_SERVER_URL;
 }
