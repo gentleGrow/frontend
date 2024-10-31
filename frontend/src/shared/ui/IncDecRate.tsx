13c26400fc52a7983a49d8465103c1727b34752a
KimYouGyoung <tptkds12@gmail.com>
2024-10-31 19:58:50 +0900

    fix: 소수점 셋쨰자리에서 올림하지 않는 문제 수정

--- frontend/src/shared/ui/IncDecRate.tsx
+++ frontend/src/shared/ui/IncDecRate.tsx
@@ -5,12 +5,24 @@ export default function IncDecRate({
   rate: number;
   className?: string;
 }) {
-  const roundedRate = typeof rate === "number" ? rate.toFixed(2) : "0.00";
+  // 양수는 올림, 음수는 내림하여 둘째 자리까지 표시
+  const roundedRate =
+    rate > 0
+      ? (Math.ceil(rate * 100) / 100).toFixed(2)
+      : (Math.floor(rate * 100) / 100).toFixed(2);
 
   return (
     <div
-      aria-label={`The rate is ${rate > 0 ? "increased" : rate < 0 ? "decreased" : "unchanged"} by ${rate}%`}
-      className={`flex h-[24px] min-w-[64px] items-center justify-center rounded-md py-[3px] text-body-4 ${rate === 0 ? "bg-gray-5 text-gray-50" : rate > 0 ? "bg-alert/10 text-alert" : "bg-decrease/10 text-decrease"} ${className}`}
+      aria-label={`The rate is ${
+        rate > 0 ? "increased" : rate < 0 ? "decreased" : "unchanged"
+      } by ${roundedRate}%`}
+      className={`flex h-[24px] min-w-[64px] items-center justify-center rounded-md py-[3px] text-body-4 ${
+        rate === 0
+          ? "bg-gray-5 text-gray-50"
+          : rate > 0
+            ? "bg-alert/10 text-alert"
+            : "bg-decrease/10 text-decrease"
+      } ${className}`}
     >
       {rate > 0 && "+"}
       {roundedRate}%
