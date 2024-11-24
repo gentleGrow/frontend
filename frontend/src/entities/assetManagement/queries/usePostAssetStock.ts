a3d4a35001ae771f787065f1b372115791060b47
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-11-24 15:51:49 +0900

    GROW-233 fix: 자산 관리 시트 행 삭제 API 변경사항 대응
    - 타입 시스템
    - fetch
    - 리액트 쿼리 수정

--- frontend/src/entities/assetManagement/queries/usePostAssetStock.ts
+++ frontend/src/entities/assetManagement/queries/usePostAssetStock.ts
@@ -1,29 +1,18 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { keyStore } from "@/shared/lib/query-keys";
-import {
-  postAssetStock,
-  PostAssetStockRequestBody,
-} from "@/entities/assetManagement/apis/postAssetStock";
+import { postAssetStock } from "@/entities/assetManagement/apis/postAssetStock";
 import { useSetAtom } from "jotai";
 import { lastUpdatedAtAtom } from "@/entities/assetManagement/atoms/lastUpdatedAtAtom";
-import { AssetStock } from "@/widgets/asset-management-draggable-table/types/table";
-import { cellErrorAtom } from "@/widgets/asset-management-draggable-table/atoms/cellErrorAtom";
 
 export const usePostAssetStock = () => {
   const queryClient = useQueryClient();
 
   const setLastUpdatedAt = useSetAtom(lastUpdatedAtAtom);
-  const setCellError = useSetAtom(cellErrorAtom);
 
   return useMutation({
     mutationKey: keyStore.assetStock.postAssetStock.queryKey,
-    mutationFn: async ({
-      accessToken,
-      body,
-    }: {
-      accessToken: string;
-      body: PostAssetStockRequestBody;
-    }) => postAssetStock(accessToken, body),
+    mutationFn: async ({ accessToken }: { accessToken: string }) =>
+      postAssetStock(accessToken),
 
     onSuccess: async (data, variables, _context) => {
       const response = (await data.json()) as {
@@ -33,47 +22,11 @@ export const usePostAssetStock = () => {
       };
 
       if (String(response.status_code).startsWith("2")) {
-        const id = variables.body.tempId;
-        const prevData = queryClient.getQueryData<AssetStock>(
-          keyStore.assetStock.getSummary.queryKey,
-        );
-
-        if (!prevData) {
-          return;
-        }
-
-        const notAddedRow = prevData.stock_assets.filter(
-          (stock) => stock.id < 0 && stock.id !== id,
-        );
-
         setLastUpdatedAt(new Date());
+
         await queryClient.invalidateQueries({
           queryKey: keyStore.assetStock.getSummary.queryKey,
           exact: true,
-        });
-
-        queryClient.setQueryData<AssetStock>(
-          keyStore.assetStock.getSummary.queryKey,
-          () => {
-            const currentData = queryClient.getQueryData<AssetStock>(
-              keyStore.assetStock.getSummary.queryKey,
-            );
-
-            if (!currentData) {
-              return;
-            }
-
-            return {
-              ...currentData,
-              stock_assets: [...currentData.stock_assets, ...notAddedRow],
-            };
-          },
-        );
-      } else {
-        setCellError({
-          rowId: variables.body.tempId,
-          field: response.field,
-          message: response.content,
         });
       }
     },
