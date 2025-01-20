4a3babc6f85d87ca77accfa68307c606942d0482
CHOI GIHOAWN <rlghks3004@gmail.com>
2025-01-20 21:43:55 +0900

    feat(api): Axios 기반으로 자산 주식 데이터 API 호출 개선
    - `axios` 패키지를 의존성에 추가하고 기존 fetch 요청을 Axios로 교체했습니다.
    - 중복된 유틸리티 함수 `getServiceUrl`을 제거하여 코드 간소화 및 유지보수를 개선했습니다.
    - Axios를 통한 에러 핸들링 및 응답 데이터를 보다 직관적으로 처리하도록 수정했습니다.

--- frontend/src/shared/utils/getServiceUrl.ts
+++
@@ -1,8 +0,0 @@
-import { SERVICE_SERVER_URL } from "@/shared";
-
-export const getServiceUrl = () => {
-  // if (typeof window !== "undefined") {
-  //   return "";
-  // }
-  return SERVICE_SERVER_URL;
-};
