4788eee4a38c7318191b1138dc3d5ff982d01c72
CHOI GIHOAWN <rlghks3004@gmail.com>
2025-01-29 12:59:46 +0900

    fix(SummaryCard): 음수 반환시 - 기호가 하나 더 붙는 문제 수정

--- frontend/src/widgets/Summary/ui/SummaryCard.tsx
+++ frontend/src/widgets/Summary/ui/SummaryCard.tsx
@@ -21,7 +21,7 @@ export default function SummaryCard({ title, amount, rate }: SummaryCardProps) {
               <span
                 className={`text-heading-1 text-alert ${amount === 0 && "text-gray-100"} ${amount && amount > 0 && "text-alert"} ${amount && amount < 0 && "text-decrease"}`}
               >
-                {amount && amount > 0 ? "+" : amount && amount < 0 ? "-" : ""}₩
+                {amount && amount > 0 && "+"}₩
                 {commaizeNumber(amount?.toFixed(0) ?? "0")}
               </span>
             </div>
--- frontend/src/widgets/Summary/ui/SummaryCardMobile.tsx
+++ frontend/src/widgets/Summary/ui/SummaryCardMobile.tsx
@@ -25,8 +25,7 @@ export default function SummaryCardMobile({
           <span
             className={`font-bold ${increaseAmount === 0 && "text-gray-100"} ${increaseAmount > 0 && "text-alert"} ${increaseAmount < 0 && "text-decrease"}`}
           >
-            {increaseAmount > 0 ? "+" : increaseAmount < 0 ? "-" : ""}₩
-            {increaseAmount.toFixed(0)}
+            {increaseAmount > 0 && "+"}₩{increaseAmount.toFixed(0)}
           </span>
         </div>
         <div className="mt-[27px] flex flex-col space-y-[8px]">
