bd104b62cec9a72468a94786d6265374787cb845
CHOI GIHOAWN <rlghks3004@gmail.com>
2025-02-02 19:40:35 +0900

    refactor(market-index): 배너 패딩 제거 및 애니메이션 플리커링 문제 해결

--- frontend/src/widgets/market-index/ui/MarketIndexes.tsx
+++ frontend/src/widgets/market-index/ui/MarketIndexes.tsx
@@ -16,16 +16,14 @@ export default async function MarketIndexes() {
   ));
 
   return (
-    <div className="relative h-[95px] w-full items-center overflow-hidden rounded-md border border-gray-20 bg-gray-0 mobile:rounded-none mobile:border-none">
-      <div className="flex h-full items-center px-[16px]">
-        {indexes.length !== 0 ? (
-          <SwiperBox items={marketIndexItems} />
-        ) : (
-          <p className="flex w-full items-center justify-center text-sm">
-            주식 지수 정보를 가져오지 못했어요.
-          </p>
-        )}
-      </div>
+    <div className="flex h-[95px] w-full flex-row items-center overflow-hidden rounded-md border border-gray-20 bg-gray-0 mobile:rounded-none mobile:border-none">
+      {indexes.length !== 0 ? (
+        <SwiperBox items={marketIndexItems} />
+      ) : (
+        <p className="flex w-full items-center justify-center text-sm">
+          주식 지수 정보를 가져오지 못했어요.
+        </p>
+      )}
     </div>
   );
 }
