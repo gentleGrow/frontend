50e3332704ea3253e98f2348d2b649deafd8852f
CHOI GIHOAWN <rlghks3004@gmail.com>
2025-02-07 12:43:36 +0900

    HOTFIX! href를 통한 리디렉션 방식으로 로그인 방식 수정

--- frontend/src/features/login/KakaoLoginButton.tsx
+++ frontend/src/features/login/KakaoLoginButton.tsx
@@ -3,33 +3,30 @@ import { clientEnv } from "@/shared/config/client-env";
 
 export default function KakaoLoginButton() {
   return (
-    <form
-      className="w-full"
-      action={`https://kauth.kakao.com/oauth/authorize?client_id=${clientEnv.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${clientEnv.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code&scope=openid`}
-      method="GET"
+    <LineButton
+      title="카카오로 계속하기"
+      onClick={() => {
+        window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${clientEnv.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${clientEnv.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code&scope=openid`;
+      }}
+      props={{
+        className: "bg-[#FEE500] border-[#FEE500]",
+      }}
     >
-      <LineButton
-        title="카카오로 계속하기"
-        props={{
-          className: "bg-[#FEE500] border-[#FEE500]",
-        }}
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        <svg
-          width="20"
-          height="20"
-          viewBox="0 0 20 20"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M9.75 0.62085C4.72667 0.62085 0.25 4.0964 0.25 8.38363C0.25 11.0492 1.98167 13.4003 4.61833 14.7981L3.50889 18.8703C3.41056 19.2308 3.82056 19.5175 4.135 19.3086L8.99833 16.0836C9.40833 16.1231 9.82556 16.1464 9.75 16.1464C15.7728 16.1464 19.75 12.6703 19.75 8.38363C19.75 4.0964 15.7728 0.62085 9.75 0.62085Z"
-            fill="black"
-            fillOpacity="0.9"
-          />
-        </svg>
-      </LineButton>
-    </form>
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M9.75 0.62085C4.72667 0.62085 0.25 4.0964 0.25 8.38363C0.25 11.0492 1.98167 13.4003 4.61833 14.7981L3.50889 18.8703C3.41056 19.2308 3.82056 19.5175 4.135 19.3086L8.99833 16.0836C9.40833 16.1231 9.82556 16.1464 9.75 16.1464C15.7728 16.1464 19.75 12.6703 19.75 8.38363C19.75 4.0964 15.7728 0.62085 9.75 0.62085Z"
+          fill="black"
+          fillOpacity="0.9"
+        />
+      </svg>
+    </LineButton>
   );
 }
--- frontend/src/features/login/NaverLoginButton.tsx
+++ frontend/src/features/login/NaverLoginButton.tsx
@@ -3,30 +3,27 @@ import { LineButton } from "@/shared";
 
 export default function NaverLoginButton() {
   return (
-    <form
-      action={`https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${clientEnv.NEXT_PUBLIC_NAVER_CLIENT_ID}&redirect_uri=${clientEnv.NEXT_PUBLIC_NAVER_REDIRECT_URI}&state=${clientEnv.NEXT_PUBLIC_NAVER_STATE_STRING}`}
-      method="GET"
-      className="w-full"
+    <LineButton
+      title="네이버로 계속하기"
+      onClick={() => {
+        window.location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${clientEnv.NEXT_PUBLIC_NAVER_CLIENT_ID}&redirect_uri=${clientEnv.NEXT_PUBLIC_NAVER_REDIRECT_URI}&state=${clientEnv.NEXT_PUBLIC_NAVER_STATE_STRING}`;
+      }}
+      props={{
+        className: "bg-[#03C75A] text-white border-[#03C75A]",
+      }}
     >
-      <LineButton
-        title="네이버로 계속하기"
-        props={{
-          className: "bg-[#03C75A] text-white border-[#03C75A]",
-        }}
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        <svg
-          width="20"
-          height="20"
-          viewBox="0 0 20 20"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M13.035 10.59L6.72669 1.57019H1.50024V18.4297H6.97958V9.40986L13.2738 18.4297H18.5002V1.57019H13.035V10.59Z"
-            fill="white"
-          />
-        </svg>
-      </LineButton>
-    </form>
+        <path
+          d="M13.035 10.59L6.72669 1.57019H1.50024V18.4297H6.97958V9.40986L13.2738 18.4297H18.5002V1.57019H13.035V10.59Z"
+          fill="white"
+        />
+      </svg>
+    </LineButton>
   );
 }
