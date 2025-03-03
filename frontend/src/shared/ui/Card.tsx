b93ed165c054464c16b6567c8513d4ad6a0f550b
CHOI GIHOAWN <rlghks3004@gmail.com>
2025-03-03 15:28:38 +0900

    refactor(ui): 불필요한 스타일 및 폰트 크기 로직 제거
    - Card 컴포넌트에서 fontSize 관련 로직 제거
    - PriceDisplay 컴포넌트와 SummaryCard에서 폰트 스타일 수정 및 정리
    - 코드 가독성을 위해 불필요한 클래스명 및 구조 단순화

--- frontend/src/shared/ui/Card.tsx
+++ frontend/src/shared/ui/Card.tsx
@@ -1,20 +1,11 @@
 "use client";
 
 import { ReactNode } from "react";
-import { cn } from "@/components/ui/utils";
 import { TooltipWithIcon } from "@/shared";
-
-const fontSizesVariants = {
-  xl: "text-xl",
-  lg: "text-lg",
-  md: "text-md",
-  sm: "text-sm",
-};
 
 interface CardProps {
   children: ReactNode; // children prop의 타입 정의
   title?: string;
-  fontSize?: keyof typeof fontSizesVariants; // fontSize를 위한 prop 추가
   width?: string; // 너비를 위한 prop 추가
   height?: string; // 높이를 위한 prop 추가
   withTooltip?: boolean;
@@ -26,7 +17,6 @@ export default function Card({
   title,
   width,
   height,
-  fontSize = "md",
   withTooltip = false,
   tooltipText = "",
 }: CardProps) {
@@ -36,13 +26,8 @@ export default function Card({
       style={{ width: width, height: height }}
     >
       {title && (
-        <h2
-          className={cn(
-            "flex flex-row items-center gap-1 font-bold text-gray-80",
-            fontSizesVariants[fontSize],
-          )}
-        >
-          <span>{title}</span>
+        <h2 className="flex flex-row items-center gap-1">
+          <span className="text-heading-2 font-bold text-gray-80">{title}</span>
           {withTooltip && <TooltipWithIcon text={tooltipText} />}
         </h2>
       )}
